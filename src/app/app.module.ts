3fdc34db8a3e35e7b39bf9747bfb55b9179885b6
Jose Maria Vences <jose.vencar.vences@gmail.com>
2022-07-25 20:20:46 -0500

    Modulo de contador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './components/contador/contador/contador.component';
+
+import { ContadorModule } from './components/contador/contador/contador.module';
 import { HeroesModule } from './components/heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
