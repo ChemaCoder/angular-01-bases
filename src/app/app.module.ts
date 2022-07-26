e908dbb693a1fe8b104e5df45f96125f77321dae
Jose Maria Vences <jose.vencar.vences@gmail.com>
2022-07-25 19:05:16 -0500

    Creacion de listado de heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './components/contador/contador.component';
 import { HeroComponent } from './components/heroes/hero/hero.component';
+import { ListadoComponent } from './components/heroes/listado/listado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContadorComponent,
-    HeroComponent
+    HeroComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule
